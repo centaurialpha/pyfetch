fb6967da97e83a23d7bc3f4f22c43e67133f839f
Gabriel Acosta <acostadariogabriel@gmail.com>
2021-05-22 19:41:11 -0300

    First version stable

--- src/pyfetch/main.py
+++ src/pyfetch/main.py
@@ -1,2 +1,28 @@
+import enum
+
+from pyfetch.utils import (
+    get_user_hostname_info,
+    get_memory_info,
+    get_cores,
+)
+
+
+class Colors:
+    BLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+
+
 def run():
-    print('diaiii')
+    print(Colors.BLUE, end='')
+    print(get_user_hostname_info())
+    print(Colors.RESET, end='')
+    print()
+    print(Colors.GREEN, end='')
+    print(f'Memory: {get_memory_info()}')
+    print(Colors.RESET, end='')
+    print(Colors.YELLOW, end='')
+    print(f'Cores: {get_cores()}')
+    print(Colors.RESET, end='')
---
+++ src/pyfetch/utils.py
@@ -0,0 +1,51 @@
+import os
+import psutil
+
+
+def get_username() -> str:
+    return os.getenv('USER')
+
+
+def get_hostname() -> str:
+    host_filename = '/etc/hostname'
+    with open(host_filename) as host_file:
+        return host_file.read().strip()
+
+
+def get_user_hostname_info() -> str:
+    user = get_username()
+    host = get_hostname()
+    return f'{user}@{host}'
+
+
+def _get_memory():
+    return psutil.virtual_memory()
+
+
+def _convert_to_gb(value):
+    return value / (1024**3)
+
+
+def _convert_to_mb(value):
+    return value / (1024**2)
+
+
+def get_used_memory():
+    mem_obj = _get_memory()
+    used = mem_obj.used
+    return round(_convert_to_gb(used), 2)
+
+def get_total_memory():
+    mem_obj = _get_memory()
+    total = mem_obj.total
+    return round(_convert_to_gb(total), 2)
+
+
+def get_memory_info() -> str:
+    used_mem = get_used_memory()
+    total_mem = get_total_memory()
+    return f'{used_mem}GB / {total_mem}GB'
+
+
+def get_cores() -> str:
+    return psutil.cpu_count()
